ca341be2f2ea22233f0bd194eeea5c841d3d3aac
Rachel Sharp <rachel@hashicorp.com>
2021-07-27 23:53:22 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 
 output "ip_address" {
-  value = digitalocean_droplet.terramino[*].ipv4_address
+  value = digitalocean_droplet.terramino.ipv4_address
   description = "The public IP address of your droplet application."
 }
